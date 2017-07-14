86338970030b513737e09304b9dca1cf37dc8875
ccSunday <ccSunday@173-16-21-91.client.mchsi.com>
2017-07-14 09:52:23 +0800

    提交文件

---
+++ GitSwitchEnvironment/GitSwitchEnvironment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitSwitchEnvironment
+//
+//  Created by ccSunday on 2017/7/14.
+//  Copyright © 2017年 ccSunday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
